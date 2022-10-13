73bf775be612113902948e723642384b1d643e14
Hovstadius <115700333+Hovstadius@users.noreply.github.com>
2022-10-13 16:25:51 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -2,6 +2,10 @@
 
 from person import Person
 from numba import jit
+from time import perf_counter as pc
+from time import sleep as pause
+import matplotlib as plt
+
 
 
 def fib_py(n):
@@ -19,15 +23,53 @@ def fib_numba(n):
 		n = n - 1
 	return c	
 
+def time(n,func):
+	times = []
+	for i in n:
+		start = pc()
+		func(i)
+		end = pc()
+		times += (end-start)
+	return times
+
 
 def main():
-	f = Person(5)
-	print(f.get())
-	f.set(10)
-	print(f.get())
-	print(f.fib())
-	print(fib_numba(10))
-	print(fib_py(10))
+	
+	nlist = [i for i in range(30,45)]
+	f = Person(20)
+
+	py_times = []
+	num_times = []
+	cpp_times = []
+
+	print('Starting fib_py')
+	py_times = time(nlist, fib_py)
+	print('Done')
+
+	print('Starting fib_numba')
+	num_times = time(nlist, fib_numba)
+	print('Done')
+
+	print('Starting fib_cpp')
+	cpp_times = time(nlist, f.fib)
+	print('Done')
+
+	plt.plot(nlist,py_times)
+	plt.plot(nlist,num_times)
+	plt.plot(nlist,cpp_times)
+	plt.legend(['fib_py','fib_numba','fib_cpp'])
+	plt.xlabel('n')
+	plt.ylabel('Time [s]')
+	plt.savefig('Times')
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	main()
